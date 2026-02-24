57a93503102fde3076c5897c47cdc98c5836ff7d
Jack Merkl <jmerkl@troy.edu>
2026-02-24 11:01:24 -0600

    Initial commit

---
+++ GoblinRPG/GoblinRPG.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+    cout << "WELCOME TO....\n" << " \n";
+
+    cout << R"(
+             888     888d8b         
+                888     888Y8P         
+                888     888            
+ .d88b.  .d88b. 88888b. 88888888888b.  
+d88P"88bd88""88b888 "88b888888888 "88b 
+888  888888  888888  888888888888  888 
+Y88b 888Y88..88P888 d88P888888888  888 
+ "Y88888 "Y88P" 88888P" 888888888  888 
+     888                               
+Y8b d88P                               
+ "Y88P"                               )" << " \n" << " \n" << "Press ENTER to continue...";
+
+    cin.get();
+    system("cls");
+    cout << "Welcome initiate.";
+
+
+
+
+   
+}
