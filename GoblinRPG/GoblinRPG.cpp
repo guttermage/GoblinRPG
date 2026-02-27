0bcff4b7444537a57618c37711f307c077523e8c
Jack Merkl <jmerkl@troy.edu>
2026-02-27 15:14:53 -0600

    Add room struct and test room at beginning

--- GoblinRPG/GoblinRPG.cpp
+++ GoblinRPG/GoblinRPG.cpp
@@ -21,8 +21,23 @@ struct NPC {
     bool isAlive = true;
 };
 
+//Holds info variables for the dungeon vector
+struct Rooms {
+    std::string name;
+    std::string description;
+    std::vector<int> exits;
+};
 int main()
 {
+    //Sets the opening scene
+    int currentRoom = 0;
+
+    //Describes variables that were defined in the Rooms struct.
+    std::vector<Rooms> dungeon = {
+        {"Birthing Pools", "Muddy, stinky, and loud. Welcome to life.",{1,2}}
+    };
+
+    //Defines a "Player" variable named user so that we can call the name later.
     Player user;
 
     std::cout << "WELCOME TO....\n" << " \n";
@@ -36,13 +51,38 @@ o888    88  o888   888o 888    888 888         888   8888o  88        888    888
                                                                                                          
 )"  << " \n" << " \n" << "Press ENTER to continue...";
 
+    //Need to change this so it doesn't exclusively work on Windows.
     std::cin.get();
     system("cls");
 
-    std::cout << "Welcome, initiate. State thy name: \n \n";
+    //Location explanation
+    std::cout << "LOCATION: " << dungeon[currentRoom].name << "\n";
+    std::cout << dungeon[currentRoom].description << "\n\n";
+
+    std::cout << R"(
+  Far beneath the soils of man, new life is made in the subterranean mud pits of Gobliwagwan.
+  You breathe your first breaths and eat your first mudpie as you aspirate on the coating of
+  goblin slurry covering your face. Wiping your eyes clean, you notice other creatures around you
+  reacting similarly. The air reeks of sulfur and is filled with the confused wails of what seem to be your peers.
+)" << "\n\n";
+
+    std::cout << "What are you going to do?\n\n";
+
+    std::cout << "Welcome, stinky. State thy name: \n\n";
+    //Lets you enter your name and ignores the whitespaces so there's no leak.
     std::getline(std::cin >> std:: ws, user.name);
-    std::cout << "\n" << user.name << ", for what reason do you require our counsel?";
+    std::cout << "\n" << user.name << ", for what reason do you require our counsel?\n\n";
 
+    std::cout << "Where will you go?\n\nChoose your path: \n";
+        for (int pathID : dungeon[currentRoom].exits) {
+            std::cout << "Path " << pathID << " \n";
+        }
+    std::cout << "\n";
 
+    int choice;
+    std::cin >> choice;
+
+    currentRoom = choice;
+    std::cout << "You have entered path  " << currentRoom;
 
 };
